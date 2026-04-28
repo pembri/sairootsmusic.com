60058f754d0237ef5ae5ad604c7166a050f02d9c
pembri <pembriahmad@gmail.com>
2026-04-28 21:04:08 +0700

    Update DB for Lirik Dari Jauh

--- database.js
+++ database.js
@@ -15,6 +15,13 @@ const sairootsDB = {
     ],
     "lyrics": [
         {
+            "id": "lirik-dari-jauh",
+            "title": "Lirik Dari Jauh",
+            "category": "Single",
+            "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
             "id": "lirik-jurus-liar",
             "title": "Lirik Jurus Liar",
             "category": "Single",
