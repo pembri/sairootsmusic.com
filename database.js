3e387b9c9720015fcbd31707a93a1af60e672c7b
pembri <pembriahmad@gmail.com>
2026-04-28 16:59:34 +0700

    Update via GitHub Manager

--- database.js
+++ database.js
@@ -2,12 +2,18 @@
 // File ini akan di-update otomatis oleh admin_post.html
 
 const sairootsDB = {
-    "articles": [],
-    "lyrics": [],
-    "discography": []
+    // 1. DATA ARTIKEL
+    articles: [],
+
+    // 2. DATA LIRIK
+    lyrics: [],
+
+    // 3. DATA DISKOGRAFI
+    discography: []
 };
 
 // === DATA PENCARIAN GLOBAL (FUSE.JS) ===
+// Mesin yang menggabungkan seluruh data di atas agar bisa dicari dari menu Hamburger
 const searchData = [
     ...sairootsDB.articles.map(a => ({ type: "Article", title: a.title, url: `/article/${a.id}`, excerpt: a.excerpt, category: a.category })),
     ...sairootsDB.lyrics.map(l => ({ type: "Lyric", title: `${l.title} (Lyrics)`, url: `/lyric/${l.id}`, excerpt: l.excerpt, category: l.category })),
