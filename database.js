f13dadfe69e0bbbea9b6180d6a073bd81eafe1d2
pembri <pembriahmad@gmail.com>
2026-04-28 21:02:06 +0700

    Update DB for Lirik Jurus Liar

--- database.js
+++ database.js
@@ -15,6 +15,13 @@ const sairootsDB = {
     ],
     "lyrics": [
         {
+            "id": "lirik-jurus-liar",
+            "title": "Lirik Jurus Liar",
+            "category": "Single",
+            "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
             "id": "lirik-eksploitasi",
             "title": "Lirik Eksploitasi",
             "category": "Single",
