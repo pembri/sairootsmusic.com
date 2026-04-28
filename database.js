bfbe0b7af981e10227578ff26cbf874086089764
pembri <pembriahmad@gmail.com>
2026-04-28 22:26:10 +0700

    Update lyrics: Lirik Skank Patrol

--- database.js
+++ database.js
@@ -18,7 +18,7 @@ const sairootsDB = {
             "id": "lirik-skank-patrol",
             "title": "Lirik Skank Patrol",
             "category": "Album - Critical Fire",
-            "image": "/asset_gambar/default.jpg",
+            "image": "/asset_gambar/art-critical-fire.jpg",
             "excerpt": ""
         },
         {
