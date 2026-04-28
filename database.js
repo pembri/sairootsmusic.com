75cd30cd5383fbcae605524e3e98c6320bb496a7
pembri <pembriahmad@gmail.com>
2026-04-28 21:07:07 +0700

    Update DB for Lirik Genjer Hijau

--- database.js
+++ database.js
@@ -15,6 +15,13 @@ const sairootsDB = {
     ],
     "lyrics": [
         {
+            "id": "lirik-genjer-hijau",
+            "title": "Lirik Genjer Hijau",
+            "category": "Single",
+            "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
             "id": "lirik-kasih-mengalun",
             "title": "Lirik Kasih Mengalun",
             "category": "Single",
