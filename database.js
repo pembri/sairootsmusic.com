4a49f8c8144ecb433025a775f847dc66c17b4706
pembri <pembriahmad@gmail.com>
2026-04-28 21:29:19 +0700

    Update DB for Lirik Roots Medication

--- database.js
+++ database.js
@@ -15,6 +15,13 @@ const sairootsDB = {
     ],
     "lyrics": [
         {
+            "id": "lirik-roots-medication",
+            "title": "Lirik Roots Medication",
+            "category": "Album - Critical Fire",
+            "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
             "id": "lirik-ormas-tipu-tipu",
             "title": "Lirik Ormas Tipu-tipu",
             "category": "Album - Critical Fire",
