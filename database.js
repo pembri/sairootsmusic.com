c2823657ce938a80eaf08b0e92e2a45219b51a50
pembri <pembriahmad@gmail.com>
2026-04-29 11:56:53 +0700

    Update Database

--- database.js
+++ database.js
@@ -71,17 +71,17 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
-            "id": "lirik-blaze-it-up",
-            "title": "Lirik Blaze It Up",
-            "category": "Album - Critical Fire",
-            "image": "/asset_gambar/art-critical-fire.jpg",
-            "excerpt": ""
-        },
-        {
             "id": "lirik-genjer-hijau",
             "title": "Lirik Genjer Hijau",
             "category": "Single",
             "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
+            "id": "lirik-blaze-it-up",
+            "title": "Lirik Blaze It Up",
+            "category": "Album - Critical Fire",
+            "image": "/asset_gambar/art-critical-fire.jpg",
             "excerpt": ""
         },
         {
