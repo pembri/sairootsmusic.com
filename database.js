de724b958a9bf8def00ec7f89da1540bd64c03ae
pembri <pembriahmad@gmail.com>
2026-04-29 11:54:21 +0700

    Update Database

--- database.js
+++ database.js
@@ -36,17 +36,17 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
-            "id": "lirik-evaluasi-lagi",
-            "title": "Lirik Evaluasi Lagi",
-            "category": "Album - Critical Fire",
-            "image": "/asset_gambar/art-critical-fire.jpg",
-            "excerpt": ""
-        },
-        {
             "id": "lirik-drama-klenik",
             "title": "Lirik Drama Klenik",
             "category": "Single",
             "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
+            "id": "lirik-evaluasi-lagi",
+            "title": "Lirik Evaluasi Lagi",
+            "category": "Album - Critical Fire",
+            "image": "/asset_gambar/art-critical-fire.jpg",
             "excerpt": ""
         },
         {
