6972d86f462e25e66bd87b189d4b03a98aa34da8
pembri <pembriahmad@gmail.com>
2026-04-29 11:57:05 +0700

    Update Database

--- database.js
+++ database.js
@@ -113,17 +113,17 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
-            "id": "lirik-propaganda",
-            "title": "Lirik Propaganda",
-            "category": "Album - Critical Fire",
-            "image": "/asset_gambar/art-critical-fire.jpg",
-            "excerpt": ""
-        },
-        {
             "id": "lirik-timpang",
             "title": "Lirik Timpang",
             "category": "Single",
             "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
+            "id": "lirik-propaganda",
+            "title": "Lirik Propaganda",
+            "category": "Album - Critical Fire",
+            "image": "/asset_gambar/art-critical-fire.jpg",
             "excerpt": ""
         },
         {
