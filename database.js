a41b44c9e699d4ca0ed571cd1ba4d86e83337eb9
pembri <pembriahmad@gmail.com>
2026-04-29 11:54:23 +0700

    Update Database

--- database.js
+++ database.js
@@ -22,17 +22,17 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
-            "id": "lirik-bayang-singgasana",
-            "title": "Lirik Bayang Singgasana",
-            "category": "Album - Critical Fire",
-            "image": "/asset_gambar/art-critical-fire.jpg",
-            "excerpt": ""
-        },
-        {
             "id": "lirik-drama-klenik",
             "title": "Lirik Drama Klenik",
             "category": "Single",
             "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
+            "id": "lirik-bayang-singgasana",
+            "title": "Lirik Bayang Singgasana",
+            "category": "Album - Critical Fire",
+            "image": "/asset_gambar/art-critical-fire.jpg",
             "excerpt": ""
         },
         {
