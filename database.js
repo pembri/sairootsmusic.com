67210b190bba98aeb26e1de39a121b9a87098a16
pembri <pembriahmad@gmail.com>
2026-04-29 11:56:27 +0700

    Update Database

--- database.js
+++ database.js
@@ -78,17 +78,17 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
-            "id": "lirik-irama-hidup",
-            "title": "Lirik Irama Hidup",
-            "category": "Album - Critical Fire",
-            "image": "/asset_gambar/art-critical-fire.jpg",
-            "excerpt": ""
-        },
-        {
             "id": "lirik-kasih-mengalun",
             "title": "Lirik Kasih Mengalun",
             "category": "Single",
             "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
+            "id": "lirik-irama-hidup",
+            "title": "Lirik Irama Hidup",
+            "category": "Album - Critical Fire",
+            "image": "/asset_gambar/art-critical-fire.jpg",
             "excerpt": ""
         },
         {
