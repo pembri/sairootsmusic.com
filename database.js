58bec7060f5c103d700b700f022bf6cd7c167e51
pembri <pembriahmad@gmail.com>
2026-04-29 11:56:36 +0700

    Update Database

--- database.js
+++ database.js
@@ -29,15 +29,15 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
-            "id": "lirik-eksploitasi",
-            "title": "Lirik Eksploitasi",
+            "id": "lirik-kasih-mengalun",
+            "title": "Lirik Kasih Mengalun",
             "category": "Single",
             "image": "/asset_gambar/default.jpg",
             "excerpt": ""
         },
         {
-            "id": "lirik-kasih-mengalun",
-            "title": "Lirik Kasih Mengalun",
+            "id": "lirik-eksploitasi",
+            "title": "Lirik Eksploitasi",
             "category": "Single",
             "image": "/asset_gambar/default.jpg",
             "excerpt": ""
