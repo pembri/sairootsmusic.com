2c442c2590c74822213f566aa2e54e071f7e96e5
pembri <pembriahmad@gmail.com>
2026-04-29 11:50:43 +0700

    Update Database

--- database.js
+++ database.js
@@ -57,15 +57,15 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
-            "id": "lirik-roots-medication",
-            "title": "Lirik Roots Medication",
+            "id": "lirik-jamaican-sound",
+            "title": "Lirik Jamaican Sound",
             "category": "Album - Critical Fire",
             "image": "/asset_gambar/default.jpg",
             "excerpt": ""
         },
         {
-            "id": "lirik-jamaican-sound",
-            "title": "Lirik Jamaican Sound",
+            "id": "lirik-roots-medication",
+            "title": "Lirik Roots Medication",
             "category": "Album - Critical Fire",
             "image": "/asset_gambar/default.jpg",
             "excerpt": ""
