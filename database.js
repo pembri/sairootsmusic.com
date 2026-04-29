4a69eb3351d3ab4ed57c23922882b95c10ab9497
pembri <pembriahmad@gmail.com>
2026-04-29 11:48:48 +0700

    Update Database

--- database.js
+++ database.js
@@ -50,17 +50,17 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
-            "id": "lirik-skank-patrol",
-            "title": "Lirik Skank Patrol",
-            "category": "Album - Critical Fire",
-            "image": "/asset_gambar/art-critical-fire.jpg",
-            "excerpt": ""
-        },
-        {
             "id": "lirik-blaze-it-up",
             "title": "Lirik Blaze It Up",
             "category": "Album - Critical Fire",
             "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
+            "id": "lirik-skank-patrol",
+            "title": "Lirik Skank Patrol",
+            "category": "Album - Critical Fire",
+            "image": "/asset_gambar/art-critical-fire.jpg",
             "excerpt": ""
         },
         {
