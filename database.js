9bfe35c3c325494b16403674a8ac41a01c40db9f
pembri <pembriahmad@gmail.com>
2026-04-29 11:54:38 +0700

    Update Database

--- database.js
+++ database.js
@@ -64,6 +64,13 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
+            "id": "lirik-senandung-manis",
+            "title": "Lirik Senandung Manis",
+            "category": "Single",
+            "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
             "id": "lirik-ormas-tipu-tipu",
             "title": "Lirik Ormas Tipu-tipu",
             "category": "Album - Critical Fire",
@@ -75,13 +82,6 @@ const sairootsDB = {
             "title": "Lirik Propaganda",
             "category": "Album - Critical Fire",
             "image": "/asset_gambar/art-critical-fire.jpg",
-            "excerpt": ""
-        },
-        {
-            "id": "lirik-senandung-manis",
-            "title": "Lirik Senandung Manis",
-            "category": "Single",
-            "image": "/asset_gambar/default.jpg",
             "excerpt": ""
         },
         {
