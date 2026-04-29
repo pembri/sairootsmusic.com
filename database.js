f6c15c5bbffb5c35ec78135738da01c7289fe5b1
pembri <pembriahmad@gmail.com>
2026-04-29 11:57:13 +0700

    Update Database

--- database.js
+++ database.js
@@ -71,17 +71,17 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
-            "id": "lirik-bayang-singgasana",
-            "title": "Lirik Bayang Singgasana",
-            "category": "Album - Critical Fire",
-            "image": "/asset_gambar/art-critical-fire.jpg",
-            "excerpt": ""
-        },
-        {
             "id": "lirik-timpang",
             "title": "Lirik Timpang",
             "category": "Single",
             "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
+            "id": "lirik-bayang-singgasana",
+            "title": "Lirik Bayang Singgasana",
+            "category": "Album - Critical Fire",
+            "image": "/asset_gambar/art-critical-fire.jpg",
             "excerpt": ""
         },
         {
