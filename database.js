476a77f0c51f26e26f8bf07f8ad238ff95aec980
pembri <pembriahmad@gmail.com>
2026-04-29 11:54:40 +0700

    Update Database

--- database.js
+++ database.js
@@ -57,17 +57,17 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
-            "id": "lirik-jamaican-sound",
-            "title": "Lirik Jamaican Sound",
-            "category": "Album - Critical Fire",
-            "image": "/asset_gambar/art-critical-fire.jpg",
-            "excerpt": ""
-        },
-        {
             "id": "lirik-senandung-manis",
             "title": "Lirik Senandung Manis",
             "category": "Single",
             "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
+            "id": "lirik-jamaican-sound",
+            "title": "Lirik Jamaican Sound",
+            "category": "Album - Critical Fire",
+            "image": "/asset_gambar/art-critical-fire.jpg",
             "excerpt": ""
         },
         {
