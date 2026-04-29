06600df4879d33bd40040ebf139e4c6dcbc51d3f
pembri <pembriahmad@gmail.com>
2026-04-29 11:57:04 +0700

    Update Database

--- database.js
+++ database.js
@@ -120,17 +120,17 @@ const sairootsDB = {
             "excerpt": ""
         },
         {
-            "id": "lirik-roots-medication",
-            "title": "Lirik Roots Medication",
-            "category": "Album - Critical Fire",
-            "image": "/asset_gambar/art-critical-fire.jpg",
-            "excerpt": ""
-        },
-        {
             "id": "lirik-timpang",
             "title": "Lirik Timpang",
             "category": "Single",
             "image": "/asset_gambar/default.jpg",
+            "excerpt": ""
+        },
+        {
+            "id": "lirik-roots-medication",
+            "title": "Lirik Roots Medication",
+            "category": "Album - Critical Fire",
+            "image": "/asset_gambar/art-critical-fire.jpg",
             "excerpt": ""
         },
         {
