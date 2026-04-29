b3a9d9cb89490b66d0a1da1349bf4e358496ef4e
pembri <pembriahmad@gmail.com>
2026-04-29 11:58:22 +0700

    Update lyrics: Lirik Dari Jauh

--- database.js
+++ database.js
@@ -39,7 +39,7 @@ const sairootsDB = {
             "id": "lirik-dari-jauh",
             "title": "Lirik Dari Jauh",
             "category": "Single",
-            "image": "/asset_gambar/default.jpg",
+            "image": "/asset_gambar/dari-jauh.png",
             "excerpt": ""
         },
         {
