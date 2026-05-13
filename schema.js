010c852436ef2b6d74c51be9f0043ac7ebe65a81
pembri <pembriahmad@gmail.com>
2026-05-14 02:27:17 +0700

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,4 +1,4 @@
-// Data Schema Universal untuk SAI Roots (Versi Anti-Bentrok + Wikidata Optimized)
+// Data Schema Universal untuk SAI Roots (Versi Anti-Bentrok + Wikidata Enhanced)
 (function() {
   const schemaData = {
     "@context": "https://schema.org",
@@ -22,10 +22,9 @@
         "description": "SAI Roots adalah proyek musik independen beraliran Reggae, Rap, dan Ska asal Indonesia, diciptakan dan dikelola secara eksklusif oleh Ahmad Supembri.",
         "url": "https://sairootsmusic.com/",
         "image": [
-          "https://sairootsmusic.com/asset_gambar/sairoots.jpg",
-          "https://upload.wikimedia.org/wikipedia/commons/0/03/SAI_Roots.jpg"  // Wikimedia version
+          "https://upload.wikimedia.org/wikipedia/commons/0/03/SAI_Roots.jpg",
         ],
-        "genre": ["Reggae", "Rap", "Ska", "Roots Reggae", "Indonesian Reggae"],
+        "genre": ["Reggae", "Roots Reggae", "Rap", "Ska", "Hip Hop"],
         "countryOfOrigin": {
           "@type": "Country",
           "name": "Indonesia"
@@ -37,14 +36,15 @@
           "@id": "https://sairootsmusic.com/#founder"
         },
         "recordLabel": {
-          "@id": "https://sairootsmusic.com/#recordlabel"
+          "@id": "https://sairootsmusic.com/#recordLabel"
         },
         "sameAs": [
           "https://www.wikidata.org/wiki/Q139783618",
-          "https://www.youtube.com/@sairoots",
-          "https://youtube.com/channel/UCnjRvftn7QD2kDqLHszImsA",
+          "https://musicbrainz.org/artist/acd09c55-df9b-4628-9605-446b3cf6554a",
           "https://open.spotify.com/artist/0xOPCtZvhXVd9RTbU1aYTr",
           "https://music.apple.com/id/artist/sai-roots/1892187423",
+          "https://www.youtube.com/@sairoots",
+          "https://youtube.com/channel/UCnjRvftn7QD2kDqLHszImsA",
           "https://m.soundcloud.com/sairoots",
           "https://music.amazon.com/artists/B0GTFWNC32/sai-roots"
         ]
@@ -53,9 +53,14 @@
         "@type": "Person",
         "@id": "https://sairootsmusic.com/#founder",
         "name": "Ahmad Supembri",
-        "description": "Indonesian Musician, Composer, Lyricist, and Record Producer.",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/2/23/Ahmad_Supembri.jpg",
+        "description": "Indonesian musician, composer, lyricist, and record producer.",
         "url": "https://sairootsmusic.com/",
-        "image": "https://commons.wikimedia.org/wiki/File:Ahmad_Supembri.jpg", // jika ada
+        "nationality": {
+          "@type": "Country",
+          "name": "Indonesia"
+        },
+        "occupation": ["Musician", "Composer", "Lyricist", "Record Producer"],
         "sameAs": [
           "https://www.wikidata.org/wiki/Q139784101",
           "https://instagram.com/ahmadsupembri",
@@ -65,21 +70,19 @@
       },
       {
         "@type": "Organization",
-        "@id": "https://sairootsmusic.com/#recordlabel",
+        "@id": "https://sairootsmusic.com/#recordLabel",
         "name": "SAI Roots Music",
         "description": "Indonesian independent record label owned by Ahmad Supembri.",
         "url": "https://sairootsmusic.com/",
         "founder": {
           "@id": "https://sairootsmusic.com/#founder"
         },
-        "sameAs": [
-          "https://www.wikidata.org/wiki/Q139784313"
-        ]
+        "sameAs": ["https://www.wikidata.org/wiki/Q139784313"]
       }
     ]
   };
 
-  // Suntik JSON-LD ke halaman
+  // Suntik JSON-LD
   const script = document.createElement('script');
   script.type = 'application/ld+json';
   script.textContent = JSON.stringify(schemaData, null, 2);
