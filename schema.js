7c169d280c3c445590b555b46f48343159b5d388
pembri <pembriahmad@gmail.com>
2026-05-14 02:00:05 +0700

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,4 +1,4 @@
-// Data Schema Universal untuk SAI Roots (Versi Anti-Bentrok)
+// Data Schema Universal untuk SAI Roots (Versi Anti-Bentrok + Wikidata Optimized)
 (function() {
   const schemaData = {
     "@context": "https://schema.org",
@@ -8,7 +8,8 @@
         "@id": "https://sairootsmusic.com/#website",
         "url": "https://sairootsmusic.com/",
         "name": "SAI Roots Music",
-        "alternateName": "SAI Roots",
+        "alternateName": ["SAI Roots", "Sai Roots"],
+        "description": "Official website of SAI Roots - Indonesian independent Reggae, Rap & Ska music project.",
         "publisher": {
           "@id": "https://sairootsmusic.com/#organization"
         }
@@ -17,34 +18,70 @@
         "@type": "MusicGroup",
         "@id": "https://sairootsmusic.com/#organization",
         "name": "SAI Roots",
+        "alternateName": "SAI Roots Music",
         "description": "SAI Roots adalah proyek musik independen beraliran Reggae, Rap, dan Ska asal Indonesia, diciptakan dan dikelola secara eksklusif oleh Ahmad Supembri.",
         "url": "https://sairootsmusic.com/",
-        "image": "https://sairootsmusic.com/asset_gambar/sairoots.jpg",
+        "image": [
+          "https://sairootsmusic.com/asset_gambar/sairoots.jpg",
+          "https://upload.wikimedia.org/wikipedia/commons/0/03/SAI_Roots.jpg"  // Wikimedia version
+        ],
+        "genre": ["Reggae", "Rap", "Ska", "Roots Reggae", "Indonesian Reggae"],
+        "countryOfOrigin": {
+          "@type": "Country",
+          "name": "Indonesia"
+        },
         "founder": {
-          "@type": "Person",
-          "name": "Ahmad Supembri"
+          "@id": "https://sairootsmusic.com/#founder"
+        },
+        "member": {
+          "@id": "https://sairootsmusic.com/#founder"
+        },
+        "recordLabel": {
+          "@id": "https://sairootsmusic.com/#recordlabel"
         },
         "sameAs": [
+          "https://www.wikidata.org/wiki/Q139783618",
           "https://www.youtube.com/@sairoots",
           "https://youtube.com/channel/UCnjRvftn7QD2kDqLHszImsA",
-          "https://music.youtube.com/channel/UC2EfMkjDMG9LTWfc1llL4AQ",
-          "https://music.youtube.com/channel/UCnjRvftn7QD2kDqLHszImsA",
           "https://open.spotify.com/artist/0xOPCtZvhXVd9RTbU1aYTr",
           "https://music.apple.com/id/artist/sai-roots/1892187423",
-          "https://link.deezer.com/s/3366qh6efN8QoSqSPCEsB",
           "https://m.soundcloud.com/sairoots",
-          "https://music.amazon.com/artists/B0GTFWNC32/sai-roots",
+          "https://music.amazon.com/artists/B0GTFWNC32/sai-roots"
+        ]
+      },
+      {
+        "@type": "Person",
+        "@id": "https://sairootsmusic.com/#founder",
+        "name": "Ahmad Supembri",
+        "description": "Indonesian Musician, Composer, Lyricist, and Record Producer.",
+        "url": "https://sairootsmusic.com/",
+        "image": "https://commons.wikimedia.org/wiki/File:Ahmad_Supembri.jpg", // jika ada
+        "sameAs": [
+          "https://www.wikidata.org/wiki/Q139784101",
           "https://instagram.com/ahmadsupembri",
           "https://facebook.com/ahmadsupembri",
           "https://tiktok.com/@ahmadsupembri"
+        ]
+      },
+      {
+        "@type": "Organization",
+        "@id": "https://sairootsmusic.com/#recordlabel",
+        "name": "SAI Roots Music",
+        "description": "Indonesian independent record label owned by Ahmad Supembri.",
+        "url": "https://sairootsmusic.com/",
+        "founder": {
+          "@id": "https://sairootsmusic.com/#founder"
+        },
+        "sameAs": [
+          "https://www.wikidata.org/wiki/Q139784313"
         ]
       }
     ]
   };
 
-  // Logika suntik JSON-LD
+  // Suntik JSON-LD ke halaman
   const script = document.createElement('script');
   script.type = 'application/ld+json';
-  script.text = JSON.stringify(schemaData);
+  script.textContent = JSON.stringify(schemaData, null, 2);
   document.head.appendChild(script);
 })();
