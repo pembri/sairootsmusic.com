88c6789632711ea67737a48c77839d4aba789878
pembri <pembriahmad@gmail.com>
2026-05-15 02:38:48 +0700

    Generate schema for https://sairootsmusic.com/cover/one-day-matisyahu-cover-reggae-ska-by-sai-roots

---
+++ schema/cover-one-day-matisyahu-cover-reggae-ska-by-sai-roots/schema.js
@@ -0,0 +1,114 @@
+// Auto-Generated Schema by SAI Roots Schema Manager
+(function() {
+  const schema = {
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "VideoObject",
+      "@id": "https://sairootsmusic.com/cover/one-day-matisyahu-cover-reggae-ska-by-sai-roots#videoobject",
+      "url": "https://sairootsmusic.com/cover/one-day-matisyahu-cover-reggae-ska-by-sai-roots",
+      "creator": {
+        "@id": "https://sairootsmusic.com/#founder"
+      }
+    },
+    {
+      "@type": "WebSite",
+      "@id": "https://sairootsmusic.com/#website",
+      "url": "https://sairootsmusic.com/",
+      "name": "SAI Roots Music",
+      "alternateName": [
+        "SAI Roots",
+        "Sai Roots"
+      ],
+      "description": "Official website of SAI Roots - Indonesian independent Reggae, Rap & Ska music project.",
+      "publisher": {
+        "@id": "https://sairootsmusic.com/#organization"
+      }
+    },
+    {
+      "@type": "MusicGroup",
+      "@id": "https://sairootsmusic.com/#organization",
+      "name": "SAI Roots",
+      "alternateName": "SAI Roots Music",
+      "description": "SAI Roots adalah proyek musik independen beraliran Reggae, Rap, dan Ska asal Indonesia, diciptakan dan dikelola secara eksklusif oleh Ahmad Supembri.",
+      "url": "https://sairootsmusic.com/",
+      "image": [
+        "https://upload.wikimedia.org/wikipedia/commons/0/03/SAI_Roots.jpg"
+      ],
+      "genre": [
+        "Reggae",
+        "Roots Reggae",
+        "Rap",
+        "Ska",
+        "Hip Hop"
+      ],
+      "countryOfOrigin": {
+        "@type": "Country",
+        "name": "Indonesia"
+      },
+      "founder": {
+        "@id": "https://sairootsmusic.com/#founder"
+      },
+      "member": {
+        "@id": "https://sairootsmusic.com/#founder"
+      },
+      "recordLabel": {
+        "@id": "https://sairootsmusic.com/#recordLabel"
+      },
+      "sameAs": [
+        "https://www.wikidata.org/wiki/Q139783618",
+        "https://musicbrainz.org/artist/acd09c55-df9b-4628-9605-446b3cf6554a",
+        "https://www.youtube.com/@sairoots",
+        "https://open.spotify.com/artist/0xOPCtZvhXVd9RTbU1aYTr",
+        "https://music.apple.com/id/artist/1892187423",
+        "https://m.soundcloud.com/sairoots",
+        "https://www.deezer.com/artist/384508281",
+        "https://music.amazon.com/artists/B0GTFWNC32/sai-roots",
+        "https://youtube.com/channel/UCnjRvftn7QD2kDqLHszImsA"
+      ]
+    },
+    {
+      "@type": "Person",
+      "@id": "https://sairootsmusic.com/#founder",
+      "name": "Ahmad Supembri",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/2/23/Ahmad_Supembri.jpg",
+      "description": "Indonesian musician, composer, lyricist, and record producer.",
+      "url": "https://sairootsmusic.com/",
+      "nationality": {
+        "@type": "Country",
+        "name": "Indonesia"
+      },
+      "occupation": [
+        "Musician",
+        "Composer",
+        "Lyricist",
+        "Record Producer"
+      ],
+      "sameAs": [
+        "https://www.wikidata.org/wiki/Q139784101",
+        "https://instagram.com/ahmadsupembri",
+        "https://facebook.com/ahmadsupembri",
+        "https://tiktok.com/@ahmadsupembri"
+      ]
+    },
+    {
+      "@type": "Organization",
+      "@id": "https://sairootsmusic.com/#recordLabel",
+      "name": "SAI Roots Music",
+      "description": "Indonesian independent record label owned by Ahmad Supembri.",
+      "url": "https://sairootsmusic.com/",
+      "founder": {
+        "@id": "https://sairootsmusic.com/#founder"
+      },
+      "sameAs": [
+        "https://www.wikidata.org/wiki/Q139784313"
+      ]
+    }
+  ]
+};
+  const script = document.createElement('script');
+  script.type = 'application/ld+json';
+  script.id = 'sairoots-auto-schema';
+  script.textContent = JSON.stringify(schema);
+  document.head.appendChild(script);
+})();
