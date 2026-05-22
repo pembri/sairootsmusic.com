f93936e13559eff2ad49e63659074599ad1a0dc0
pembri <pembriahmad@gmail.com>
2026-05-22 23:01:52 +0700

    Update discography: Rastaman

--- database.js
+++ database.js
@@ -269,7 +269,7 @@ const sairootsDB = {
             "id": "rastaman",
             "title": "Rastaman",
             "category": "Album Reborn Riddim",
-            "image": "/asset_gambar/art-reborn-ridim.jpg",
+            "image": "/asset_gambar/art-reborn-riddim.jpg",
             "excerpt": "",
             "audio": "/discography/rastaman.mp3"
         },
